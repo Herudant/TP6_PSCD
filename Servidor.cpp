e081379be76dddbfd2c9539bc8074b115b5ef2e6
herudant <edalonso@protonmail.com>
2017-12-28 10:39:42 +0100

    asfhjahfs

--- Servidor.cpp
+++ Servidor.cpp
@@ -108,7 +108,7 @@ int main(int argc, char* argv[]){
 			cerr << "Parámetros mal introducidos: <puerto Servidor>" << endl;
 			exit(1);
 	}
-  
+
 	// se captura la señal de cierre para evitar que termine con ctrl + c.
 	signal(SIGINT, handler);
 
