171438dca34a6933593bb692f98130498c142fd5
Herudant <edalonso@protonmail.com>
2018-01-13 16:43:32 +0100

    ,

--- src/Cliente.cpp
+++ src/Cliente.cpp
@@ -111,7 +111,8 @@ int main(int argc, char *argv[]) {
 		while(!out){
 			//se envia una puja
 			mensaje = (AUTO) ? (to_string(rand() % 200 + 5)) : getLine_puja();
-
+			send_msg(socket_fd, socket, mensaje);
+			
 			if(mensaje == MENS_FIN_PUJA){
 				out = true;
 			}
